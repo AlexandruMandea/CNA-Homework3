2f211aec7e8e39c3a2249a9d2f3cce7ad8832099
alexandru_mandea <alexandru7300@gmail.com>
2021-05-28 19:12:52 +0300

    We put that task in a method in order to assign that method to a separate thread.

--- ChatClient/ChatClient/Program.cs
+++ ChatClient/ChatClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using Grpc.Net.Client;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
 using Service;
 using System;
 using System.Threading.Tasks;
@@ -10,17 +12,22 @@ namespace ChatClient
         private static readonly GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
         private static readonly Chat.ChatClient client = new Chat.ChatClient(channel);
 
-        static async Task Main(string[] args)
+        private static async void ServerToClient()
         {
             await Task.Run(async () =>
             {
                 var dataStream = client.ServerToClient(new Empty());
 
-                    await foreach (var message in dataStream.ResponseStream.ReadAllAsync())
-                    {
-                        Console.WriteLine(message.Message);
-                    }
+                await foreach (var message in dataStream.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(message.Message);
+                }
             });
+        }
+
+        static async Task Main(string[] args)
+        {
+            
         }
     }
 }
