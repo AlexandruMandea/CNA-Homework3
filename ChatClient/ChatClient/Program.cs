bb0e35511c1af31d02c02010c723b50e6d6deb24
alexandru_mandea <alexandru7300@gmail.com>
2021-05-28 19:04:33 +0300

    We created a task which is running in the background to maintain open the ServerToClient method from server.

--- ChatClient/ChatClient/Program.cs
+++ ChatClient/ChatClient/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using Service;
+using System;
+using System.Threading.Tasks;
 
 namespace ChatClient
 {
     class Program
     {
         private static readonly GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
-        private static readonly Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+        private static readonly Chat.ChatClient client = new Chat.ChatClient(channel);
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Task.Run(async () =>
+            {
+                var dataStream = client.ServerToClient(new Empty());
+
+                    await foreach (var message in dataStream.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine(message.Message);
+                    }
+            });
         }
     }
 }
