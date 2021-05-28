b321ff54039a353180615f365b857ee81e949ecc
alexandru_mandea <alexandru7300@gmail.com>
2021-05-28 18:16:34 +0300

    We have declared the logger.

--- ChatService/ChatService/Services/ChatService.cs
+++ ChatService/ChatService/Services/ChatService.cs
@@ -1,5 +1,7 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
+using Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,14 @@ using System.Threading.Tasks;
 
 namespace ChatService
 {
-    public class ChatService 
+    public class ChatService : Chat.ChatBase
     {
+        private readonly ILogger<ChatService> _logger;
 
+
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
     }
 }
