72b88538fe74a2d3cc28d897f5f3029a70b7450d
alexandru_mandea <alexandru7300@gmail.com>
2021-05-28 18:59:32 +0300

    We declared the channel and the client in Program class from client project.

--- ChatClient/ChatClient/Program.cs
+++ ChatClient/ChatClient/Program.cs
@@ -4,6 +4,9 @@ namespace ChatClient
 {
     class Program
     {
+        private static readonly GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+        private static readonly Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
